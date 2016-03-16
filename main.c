faa1dd293d073756c7fb66f248d27bbe9d5b4691
Alttawab <alttawab@gmail.com>
2016-03-17 01:56:42 +0200

    KeyPad library tested OK

--- main.c
+++ main.c
@@ -5,41 +5,75 @@
  */
 
 
-#include "HAL\LED.h"
-#include <util/delay.h>
-
-void delay();
+#include "HAL/LED.h"
+#include "HAL/keyPad.h"
+#include <util\delay.h>
 int main()
 {
 	void_led_init_all_led();
+	void_keyPad_init();
 	void_led_all_led_off();
+	U8_t U8_key ;
+	U8_key=0;
 	while(1)
 	{
-		delay();
-		void_led_1_on();
-		delay();
-		void_led_1_off();
-		delay();
-		void_led_2_on();
-		delay();
-		void_led_2_off();
-		delay();
-		void_led_3_on();
-		delay();
-		void_led_3_off();
-		delay();
-		void_led_4_on();
-		delay();
-		void_led_4_off();
-		delay();
-		void_led_all_led_on();
-		delay();
-		void_led_all_led_off();
+		void_keyPad_wait_keyRelease();
+		void_keyPad_wait_keyPress();
+		U8_key =U8_keyPad_scanKey();
+
+		switch(U8_key)
+
+
+		{
+		case 1:
+			void_led_all_led_off();
+			void_led_1_on();
+			break ;
+		case 2:
+			void_led_all_led_off();
+			void_led_2_on();
+			break ;
+		case 3:
+			void_led_all_led_off();
+			void_led_1_on();
+			void_led_2_on();
+			break ;
+		case 4 :
+			void_led_all_led_off();
+			void_led_3_on();
+			break ;
+		case 5:
+			void_led_all_led_off();
+			void_led_3_on();
+			void_led_1_on();
+			break ;
+		case 6:
+			void_led_all_led_off();
+			void_led_3_on();
+			void_led_2_on();
+			break ;
+		case 7:
+			void_led_all_led_off();
+			void_led_1_on();
+			void_led_2_on();
+			void_led_3_on();
+			break ;
+		case 8:
+			void_led_all_led_off();
+			void_led_4_on();
+			break ;
+		case 9:
+			void_led_all_led_off();
+			void_led_1_on();
+			void_led_4_on();
+			break ;
+		default :
+			void_led_all_led_off();
+			void_led_4_on();
+			void_led_2_on();
+		}
 
 	}
-}
 
-void delay()
-{
-	_delay_ms(1000);
+return (0);
 }
