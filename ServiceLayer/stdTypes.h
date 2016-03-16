d86c414f947c7a4468a9b2d522c10e7c6538ac15
Alttawab <alttawab@gmail.com>
2016-03-17 00:46:25 +0200

    Service Layer Folder contain stdTypes.h added

---
+++ ServiceLayer/stdTypes.h
@@ -0,0 +1,38 @@
+#ifndef ServiceLayer_stdTypes_h_
+#define ServiceLayer_stdTypes_h_
+
+typedef unsigned char BOOLEAN;
+typedef unsigned char U8_t;
+typedef signed char S8_t;
+typedef unsigned int U16_t;
+typedef signed int S16_t;
+typedef unsigned long int U32_t;
+typedef signed long int S32_t;
+typedef unsigned long long int U64_t;
+typedef signed long long int S64_t;
+typedef float FP32;
+typedef double FP64;
+
+#define REG_PINS 8
+#define BYTE S8_t
+#define UBYTE U8_t
+#define WORD S16_t
+#define UWORD U16_t
+#define LONG S32_t
+#define ULONG U32_t
+#define QWORD S64_t
+#define UQWORD U64_t
+#define TRUE 1
+#define False 0
+#define SUCCESS 1
+#define FAILURE 0
+#define SET 1
+#define CLEAR 0
+#define REG_PINS 8
+#define NULL 0
+#define OUTPUT 1
+#define INPUT 0
+#define HIGH 1
+#define LOW 0
+
+#endif
