7272e724c705fb2496c4daa549bc505b2cb9267c
Alttawab <alttawab@gmail.com>
2016-03-24 03:24:10 +0200

    Lcd functions added

--- HAL/LCD.c
+++ HAL/LCD.c
@@ -1,44 +1,40 @@
 #include"LCD.h"
 
-
-
 void void_lcd_command(U8_t U8_command)
 {
+	_delay_us(1);
+	void_lcd_busy_flag() ;
 	S8_DIO_write_port(DATA_PORT  , U8_command);
 	S8_DIO_clear_pin( CONTROL_PORT , RS);
 	S8_DIO_clear_pin( CONTROL_PORT , RW);
-	S8_DIO_set_pin( CONTROL_PORT , EN) ;
-	_delay_us(1);
-	S8_DIO_clear_pin( CONTROL_PORT , EN) ;
-	_delay_us(100);
+	void_lcd_latch_data();
+
 }
 
 
 void void_lcd_data(U8_t U8_data)
 
  {
+	void_lcd_busy_flag() ;
 	S8_DIO_write_port(DATA_PORT, U8_data);
 	S8_DIO_set_pin( CONTROL_PORT, RS);
 	S8_DIO_clear_pin( CONTROL_PORT, RW);
-	S8_DIO_set_pin( CONTROL_PORT, EN);
-	_delay_us(1);
-	S8_DIO_clear_pin( CONTROL_PORT, EN);
-	_delay_us(100);
+	void_lcd_latch_data();
 }
 
 
 
 void void_lcd_init()
+
 {
 
-	S8_DIO_init_port(DATA_PORT, 0xFF);
-	S8_DIO_init_port(CONTROL_PORT, 0xFF);
-	S8_DIO_clear_pin(CONTROL_PORT, EN);
-	_delay_us(2000);
+	S8_DIO_init_output_port_mask(DATA_PORT , 0xFF );
+	S8_DIO_init_output_port_mask(CONTROL_PORT , 0x07 );
+	S8_DIO_clear_pin(CONTROL_PORT , EN);
 	void_lcd_command(0x3C);
 	void_lcd_command(0x0F);
 	void_lcd_command(0x01);
-	_delay_us(2000);
+	_delay_us(1700);
 	void_lcd_command(0x06);
 }
 
@@ -50,6 +46,7 @@ void void_lcd_print(S8_t * S8_str)
 	{
 		void_lcd_data((S8_str[U8_i]));
 		  U8_i++ ;
+		  _delay_us(1000);
 	}
 
 }
@@ -78,3 +75,106 @@ void void_lcd_goto_xy(U8_t U8_x , U8_t U8_y)
 		void_lcd_command(0x80);
 	}
 }
+
+void void_lcd_busy_flag()  /*1 wait while lcd  is busy */
+{
+	S8_DIO_init_input_port_mask(DATA_PORT , 0x00 );  /*make data port in input mode "read"*/
+	S8_DIO_clear_pin(CONTROL_PORT , RS);
+	S8_DIO_set_pin(CONTROL_PORT , RW);
+
+	do
+	{
+		void_lcd_latch_data() ;
+
+	}while (S16_DIO_read_port_mask(DATA_PORT , 0xFF)<=0x80);
+
+	S8_DIO_init_output_port_mask(DATA_PORT , 0xFF ); /*make data port in output mode "Write"*/
+
+}
+
+/*latch function*/
+void void_lcd_latch_data()
+{
+	S8_DIO_set_pin(CONTROL_PORT  , EN);
+	asm volatile ("nop");
+	asm volatile ("nop");
+	S8_DIO_clear_pin(CONTROL_PORT , EN);
+
+}
+
+	/*write intger to string function*/
+void void_lcd_int_to_asci(S32_t S32_integer , S8_t * S8_str_ptr , U8_t U8_base)
+{
+	U8_t U8_i ;
+	U8_i =0 ;
+	bool bool_sign ;
+	bool_sign =FALSE ;
+	if(S32_integer <0)
+	{
+		S32_integer *= -1 ;
+		bool_sign = TRUE ;
+	}
+	if(S32_integer == 0 )
+	{
+		*(S8_str_ptr+U8_i)='0' ;
+				U8_i += 1;
+		*(S8_str_ptr+U8_i)='\0';
+
+	}
+
+	else
+	{
+		do
+		{
+		U8_t	U8_rem ;
+		U8_rem = S32_integer %	U8_base ;
+		*(S8_str_ptr+U8_i)=(U8_rem > 10)? (U8_rem -10)+'A' :U8_rem+'0' ;
+		S32_integer /= U8_base;
+		U8_i += 1;
+		}while(S32_integer != 0);
+		if(bool_sign && U8_base == 10)
+		{
+			*(S8_str_ptr + U8_i) = '-' ;
+			U8_i += 1;
+			*(S8_str_ptr + U8_i) = '\0' ;
+		}
+	}
+	void_reverse(S8_str_ptr , U8_i-1) ;
+
+}
+	void void_reverse(S8_t * S8_str_ptr , U8_t U8_length)
+
+	{
+		U8_t U8_i ;
+		U8_i = 0 ;
+
+		S8_t S8_temp ;
+		while(U8_i < (U8_length/2) )
+         {
+		S8_temp= *(S8_str_ptr + U8_i) ;
+		*(S8_str_ptr + U8_i) = *((S8_str_ptr + U8_length )- U8_i) ;
+		*((S8_str_ptr + U8_length )-U8_i) =   S8_temp ;
+		U8_i =U8_i+1;
+         }
+	}
+
+
+/******basic fuction******/
+
+	/*1_return home function */
+	/*add clear display function*/
+	/*blink crusor function on /off */
+	/*display light off power saver*/
+
+/****Intermediate functions*****/
+
+	/*function mode 1 line or 2 line and 4bit or 8 bit etc*/
+	/*write double function*/
+	/*rotate line */
+	/*shift line */
+	/*move line*/
+
+/******advnced function******/
+
+	/*menu functions*/
+	/*draw any symbol*/
--- HAL/LCD.h
+++ HAL/LCD.h
@@ -10,6 +10,7 @@
 #include "..\ServiceLayer\stdTypes.h"
 #include "..\MCAL\D_IO.h"
 #include "util/delay.h"
+
 #define DATA_PORT 'C'
 #define CONTROL_PORT 'B'
 #define RS 0    /*CONTROL_PORT   RS=0 instruction mode RS=1 Data mode*/
@@ -22,6 +23,11 @@ void void_lcd_data(U8_t U8_data);
 void void_lcd_init();
 void void_lcd_print(S8_t * S8_str);
 void void_lcd_goto_xy(U8_t U8_x , U8_t U8_y);
+void void_lcd_busy_flag();
+void void_lcd_latch_data();
+void void_reverse(S8_t * S8_str_ptr , U8_t U8_length);
+void void_lcd_int_to_asci(S32_t S32_integer , S8_t * S8_str_ptr , U8_t U8_base);
+
 
 
 #endif /* HAL_LCD_H_ */
