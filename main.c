df6587a86ea9c827438d8fdf508f5809e8793ccc
Alttawab <alttawab@gmail.com>
2016-03-24 03:24:54 +0200

    lcd function added tested

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #include <util\delay.h>
 #include "MCAL\D_IO.h"
 #include "HAL\LCD.h"
-
+#include "serviceLayer\stdTypes.h"
 #define SET_PIN(PORT , PIN) PORT |= (1<<PIN)
 #define RESET_PIN(PORT , PIN ) PORT &= ~(1<<PIN)
 
@@ -19,17 +19,20 @@
 int main()
 
 {
-	void_keyPad_init();
 	void_lcd_init();
-	S8_t S8_arr_keyScan [2] = {0 , '\0'}; ;
+	S8_t * S8_arr_keyScan =(S8_t *)"\0";
+	void_keyPad_init();
 	while(1)
 	{
-	void_keyPad_wait_keyRelease();
-	void_keyPad_wait_keyPress();
-	S8_arr_keyScan[0] =(S8_t)U8_keyPad_scanKey()+48;
+	 void_lcd_int_to_asci( -57855 , S8_arr_keyScan , 10);
+
+	//void_keyPad_wait_keyRelease();
+//	void_keyPad_wait_keyPress();
+//	S8_arr_keyScan[0] =(S8_t)U8_keyPad_scanKey()+48;
+
 	void_lcd_print(S8_arr_keyScan);
 
 	}
-	return (0);
-}
 
+
+}
