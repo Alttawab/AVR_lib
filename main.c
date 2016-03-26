40894384d087f9d2afdb8e941d3c6b4cad0ad9ce
Alttawab <alttawab@gmail.com>
2016-03-26 18:17:16 +0200

    LCD basic tested OK√

--- main.c
+++ main.c
@@ -11,28 +11,32 @@
 #include "MCAL\D_IO.h"
 #include "HAL\LCD.h"
 #include "serviceLayer\stdTypes.h"
-#define SET_PIN(PORT , PIN) PORT |= (1<<PIN)
-#define RESET_PIN(PORT , PIN ) PORT &= ~(1<<PIN)
-
-
-
 int main()
 
 {
 	void_lcd_init();
-	S8_t * S8_arr_keyScan =(S8_t *)"\0";
 	void_keyPad_init();
+	S8_t  S8_arr_keyScan[10] ={'\0'};
+	U8_t U8_num ;
+	U8_num = 0;
 	while(1)
 	{
-	 void_lcd_int_to_asci( -57855 , S8_arr_keyScan , 10);
+	void_keyPad_wait_keyRelease();
+	void_keyPad_wait_keyPress();
+	U8_num = U8_keyPad_scanKey() ;
 
-	//void_keyPad_wait_keyRelease();
-//	void_keyPad_wait_keyPress();
-//	S8_arr_keyScan[0] =(S8_t)U8_keyPad_scanKey()+48;
+	void_lcd_goto_xy(1 , 1);
+	void_lcd_print((S8_t *)"DEC=");
+	void_lcd_int_to_asci(U8_num, S8_arr_keyScan , 10);
+	void_lcd_goto_xy(5 , 1) ;
+	void_lcd_print(S8_arr_keyScan);
 
+	void_lcd_goto_xy(1 , 2) ;
+	void_lcd_print((S8_t *)"HEX=");
+	void_lcd_goto_xy(5 , 2) ;
+	void_lcd_int_to_asci(U8_num, S8_arr_keyScan , 16);
 	void_lcd_print(S8_arr_keyScan);
 
 	}
-
 
 }
